7cea5ef3b33cb2ebb4b8f6940fbb6144a8018bb1
bunnyhoops123 <sumukh.varmay@gmail.com>
2023-09-06 17:14:25 +0530

    chapter 24

---
+++ WEBSITE/chapter21.js
@@ -0,0 +1,11 @@
+//Funtions = Define code once and re-use it multiple times
+
+let userName = "Sumukh Varma";
+let age = 21;
+
+function wishBday(name, num){
+    console.log(name, num);
+    this.name;
+}
+
+wishBday(userName, age);
