6cfffe6aac02249860e5ececf645d1ad483ce7bc
bunnyhoops123 <sumukh.varmay@gmail.com>
2023-09-06 12:50:02 +0530

    Initial

---
+++ WEBSITE/chapter1.js
@@ -0,0 +1,16 @@
+//Printing to console and commenting
+//To run in termial type:- node <relative path of .js file> and press enter
+//Example: in Terminal-- node WEBSITE\chapter1.js
+//ENSURE node is installed before running commmand
+
+console.log("I really like pizza");
+console.log('It\s really good'); // use \ to negate the functionality
+
+//This is a comment
+/*
+This is 
+a multi line 
+comment.
+*/
+
+//Use comments to add descripitons, to be ignored by the compiler.
